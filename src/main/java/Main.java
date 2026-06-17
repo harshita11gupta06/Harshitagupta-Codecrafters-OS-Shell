929bdac254d01f9e246df1e310710cac70a1c024
harshita11gupta06 <harshita.25bcs10692@sst.scaler.com>
2026-06-17 20:19:13 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -13,12 +14,23 @@ public class Main {
             if (input.equals("exit")) {
                 break;
             } 
-            // 2. Check for echo (note the trailing space so we handle arguments)
+            // 2. Check for echo
             else if (input.startsWith("echo ")) {
                 String message = input.substring(5);
                 System.out.println(message);
             } 
-            // 3. Fallback for invalid commands
+            // 3. Check for type
+            else if (input.startsWith("type ")) {
+                String commandToCheck = input.substring(5).trim();
+                
+                // Verify if the command is a known shell builtin
+                if (commandToCheck.equals("echo") || commandToCheck.equals("exit") || commandToCheck.equals("type")) {
+                    System.out.println(commandToCheck + " is a shell builtin");
+                } else {
+                    System.out.println(commandToCheck + ": not found");
+                }
+            } 
+            // 4. Fallback for invalid commands
             else {
                 System.out.println(input + ": command not found");
             }
